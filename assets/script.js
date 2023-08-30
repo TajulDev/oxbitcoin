0fae8d02d7dba2ee821b9906c190fc461db630e2
nipaaa <akternipa941@gmail.com>
2023-08-30 19:23:01 +0600

    update

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const parentElements = document.querySelectorAll('.parent');
+document.addEventListener("DOMContentLoaded", function () {
+  const parentElements = document.querySelectorAll(".parent");
 
-    parentElements.forEach(parent => {
-        const child = parent.querySelector('.child');
-        const arrowIcon = parent.querySelector('.fa-chevron-right');
+  parentElements.forEach((parent) => {
+    const child = parent.querySelector(".child");
+    const arrowIcon = parent.querySelector(".fa-chevron-right");
 
-        parent.addEventListener('click', function() {
-            arrowIcon.classList.toggle('rotate-icon');
-            if (child.style.display === 'block') {
-                child.style.display = 'none';
-            } else {
-                child.style.display = 'block';
-            }
-        });
+    parent.addEventListener("click", function () {
+      arrowIcon.classList.toggle("rotate-icon");
+      if (child.style.display === "block") {
+        child.style.display = "none";
+      } else {
+        child.style.display = "block";
+      }
     });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var copyButton = document.querySelector(".copy_text .btn");
+  var copyContainer = document.querySelector(".copy_text");
+  var range = document.createRange();
+  range.selectNode(copyContainer);
+
+  copyButton.addEventListener("click", function () {
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+
+    try {
+      document.execCommand("copy");
+      copyButton.setAttribute("title", "Copied");
+      var tooltip = new bootstrap.Tooltip(copyButton);
+    } catch (error) {
+      console.error("Copy failed:", error);
+    }
+
+    window.getSelection().removeAllRanges();
+  });
 });
